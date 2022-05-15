3f2ab687b0212933d6912139439625a9a1cd62ef
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-15 08:05:39 -0500

    Update listprem.js

--- plugins/listprem.js
+++ plugins/listprem.js
@@ -8,9 +8,9 @@ let handler = async (m, { conn }) => {
   let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
   conn.reply(m.chat, `「 List Premium 」` + `\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, { contextInfo: { mentionedJid: prem } })
 }
-handler.help = ['premlist']
-handler.tags = ['owner']
+handler.help = ['listprem']
+handler.tags = ['premium']
 handler.command = /^(listprem|premlist)$/i
-handler.owner = true
+handler.owner = false
 
 module.exports = handler
