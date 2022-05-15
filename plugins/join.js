637ff9c157b2ffac0b96c790fef3bf275a7a445a
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-15 08:42:12 -0500

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link invalid'
     let res = await conn.groupAcceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid}`)
+    m.reply(`𝐬𝐞 𝐮𝐧𝐢𝐨 𝐜𝐨𝐧 𝐞𝐱𝐢𝐭𝐨 𝐚𝐥 𝐠𝐫𝐮𝐩𝐨 ${res.gid}`)
 }
 handler.help = ['join <chat.whatsapp.com>']
 handler.tags = ['premium']
