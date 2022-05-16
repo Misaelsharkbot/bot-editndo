fb1b6d567e04e7742997088cb74fbc7066597231
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 07:31:28 -0500

    Update jadian.js

--- plugins/jadian.js
+++ plugins/jadian.js
@@ -1,17 +1,15 @@
-let handler = async (m, { conn, participants, command, usedPrefix }) => {
-    let member = participants.map(u => u.id)
-    let orang
-    if (/ku/i.test(command)) orang = m.sender
-    else orang = member[Math.floor(Math.random() * member.length)]
-    let jodoh = member[Math.floor(Math.random() * member.length)]
-    let jawab = `@${orang.replace(/@.+/, '')} ❤️ @${jodoh.replace(/@.+/, '')}`.trim()
-    let mentionedJid = [orang, jodoh]
-    await conn.sendBut(m.chat, jawab, '', `${command}`, usedPrefix + command, m, { contextInfo: { mentionedJid } })
-}
-handler.help = ['jodohin', 'jodohku', 'jadian']
-handler.tags = ['fun']
-handler.command = /^jodoh(in|ku)|jadian$/i
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)} Y ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 ≧◠ᴥ◠≦*`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['formarpareja','formarparejas']
 handler.group = true
-handler.limit = false
-
-module.exports = handler
+export default handler
