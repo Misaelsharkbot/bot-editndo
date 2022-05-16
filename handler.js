3dd1149045bba8653156553404026109e7d968f0
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 07:28:10 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -706,9 +706,9 @@ module.exports = {
                             pp = await this.profilePictureUrl(user, 'image')
                         } catch (e) {
                         } finally {
-                            text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
-                                (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang 1'))
-                                this.sendButtonLoc(id, await(await fetch(pp)).buffer(), text, '', "Okey", "huuu", null)
+                            text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || '@user Yah,si Beban Masuk Grup').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
+                                (chat.sBye || this.bye || conn.bye || 'Sip @user , Beban Berkurang 1'))
+                                this.sendButtonLoc(id, await(await fetch(pp)).buffer(), text, '', "𝑩𝑰𝑬𝑵𝑽𝑬𝑵𝑰𝑫𝑶", "huuu", null)
                                 }
                     }
                 }
